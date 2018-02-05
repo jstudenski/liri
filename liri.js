55eeb257f5b73f416a7d8d6828357a022b5fe33f
jstudenski <jstudenski@gmail.com>
2018-02-05 10:51:44 -0600

    added case statement and help case

--- liri.js
+++ liri.js
@@ -11,6 +11,41 @@ var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+// get user input
+var param = process.argv[2];
+switch (param) {
+  case 'help':
+    console.log(colors.FgGreen+'Options:'+colors.Reset);
+    console.log('  my-tweets');  
+    console.log(colors.Dim+'  This will show your last 20 tweets and when they were created'+colors.Reset); 
+    console.log('  spotify-this-song \'<song name here>\'');  
+    console.log(colors.Dim+'  This will show the various information about a song'+colors.Reset); 
+    console.log('  movie-this \'<movie name here>\'');
+    console.log(colors.Dim+'  This will output various information about a movie'+colors.Reset);     
+    console.log('  do-what-it-says');  
+    console.log(colors.Dim+'  This will call a command from inside of random.txt'+colors.Reset);  
+    break;
+  case 'my-tweets':
+
+
+    break;
+  case 'spotify-this-song':
+
+    break;
+  case 'movie-this':
+    var movieName = process.argv[3];
+    movieThis(movieName);
+    break;
+  case 'do-what-it-says':
+
+    break;
+  default:
+    console.log(colors.FgRed+'Invalid parameter:'+colors.Reset);
+    console.log('Try: \'node liri.js help\' to see avaliable options.');    
+}
+
+//console.log(result)
+
 
 // * `my-tweets`
 
@@ -21,40 +56,34 @@ var client = new Twitter(keys.twitter);
 // * `do-what-it-says`
 
 // We then run the request module on a URL with a JSON
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-  if (!error && response.statusCode === 200) {
-    console.log("--------------------------------");
-    console.log(colors.BgCyan);
-
-    console.log("Title: "+JSON.parse(body).Title);
 
 
-    console.log("Year released: "+JSON.parse(body).Year);
-    console.log("IMDB Rating: "+JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1].Value);
+function movieThis(name){
+  var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
 
-    console.log(JSON.parse(body).Country);
-    console.log(JSON.parse(body).Language);
-    console.log(JSON.parse(body).Plot);
-    console.log(JSON.parse(body).Actors); 
+  request(queryUrl, function(error, response, body) {
 
-    console.log(colors.Reset);
-    console.log("--------------------------------");
+    // If there were no errors and the response code was 200 (i.e. the request was successful)...
+    if (!error && response.statusCode === 200) {
+      console.log("--------------------------------");
+      console.log(colors.BgCyan);
 
-//console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
+      console.log("Title: "+JSON.parse(body).Title);
+
+      console.log("Year released: "+JSON.parse(body).Year);
+      console.log("IMDB Rating: "+JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1].Value);
+
+      console.log(JSON.parse(body).Country);
+      console.log(JSON.parse(body).Language);
+      console.log(JSON.parse(body).Plot);
+      console.log(JSON.parse(body).Actors); 
+
+      console.log(colors.Reset);
+      console.log("--------------------------------");
+    }
+
+  });
+}
 
 
-  //  console.log(JSON.parse(body));
-
-    // Then we print out the imdbRating
-  //  console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  console.log('HELLO');  //yellow
-  }
-
-});
-
-// for (var key in colors) { 
-//   console.log(colors[key], 'Hello');
-// }
