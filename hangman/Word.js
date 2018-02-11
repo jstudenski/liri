fa58903815ce49185d5e3a97ffa7a03b3d9b2e44
jstudenski <jstudenski@gmail.com>
2018-02-11 16:31:18 -0600

    added prototypes to Word

--- hangman/Word.js
+++ hangman/Word.js
@@ -6,17 +6,19 @@ var Word = function(word){
   for (var x = 0; x < word.length; x++){
     this.letters.push(new Letter(word.charAt(x)));
   };
-  this.display = function(){
-    var show = ""
-    for (var x = 0; x < this.letters.length; x++){
-      show = show.concat(this.letters[x]+' ')
-    }
-    console.log(show);
-  };
-  this.guess = function(char){
-    for (var x = 0; x < this.letters.length; x++){
-      this.letters[x].makeGuess(char);
-    }
+}
+
+Word.prototype.display = function() {
+  var show = ""
+  for (var x = 0; x < this.letters.length; x++){
+    show = show.concat(this.letters[x]+' ')
+  }
+  console.log(show);
+}
+
+Word.prototype.guess = function(char) {
+  for (var x = 0; x < this.letters.length; x++){
+    this.letters[x].makeGuess(char);
   }
 }
 
