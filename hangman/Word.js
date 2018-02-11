3d8d98d11f56c2e3311e81d048af8b94a9cb5493
jstudenski <jstudenski@gmail.com>
2018-02-11 13:45:29 -0600

    fixed work constructor to add letters automatically

--- hangman/Word.js
+++ hangman/Word.js
@@ -1,11 +1,34 @@
 var Letter = require("./Letter.js");
 
+
+// var Word = function(word){
+//   this.letters = [];
+//   this.addLetters = function(word){
+//     for (var x = 0; x < word.length; x++){
+//       this.letters.push(new Letter(word.charAt(x)));
+//     }
+//   };
+//   this.userDisplay = function(){
+//     for (var x = 0; x < this.letters.length; x++){
+//       this.letters[x].output();
+//     }
+//   };
+//   this.guessLetter = function(char){
+//     for (var x = 0; x < this.letters.length; x++){
+//       this.letters[x].makeGuess(char);
+//     }
+//   }
+// }
+// function parseWord(word) {
+//   for (var x = 0; x < word.length; x++){
+//     this.push(new Letter(word.charAt(x)));
+//   }
+// }
+
 var Word = function(word){
   this.letters = [];
-  this.addLetters = function(word){
-    for (var x = 0; x < word.length; x++){
-      this.letters.push(new Letter(word.charAt(x)));
-    }
+  for (var x = 0; x < word.length; x++){
+    this.letters.push(new Letter(word.charAt(x)));
   };
   this.userDisplay = function(){
     for (var x = 0; x < this.letters.length; x++){
@@ -19,16 +42,38 @@ var Word = function(word){
   }
 }
 
-
 var myWord = new Word('test');
-myWord.addLetters('cat');
-//console.log(myWord.letters);
+
+
+console.log(myWord.letters);
 
 myWord.userDisplay();
 myWord.guessLetter('t');
 myWord.userDisplay();
-myWord.guessLetter('c');
+myWord.guessLetter('s');
 myWord.userDisplay();
 //console.log()
 
 module.exports = Word;
+
+
+
+
+
+// var Word = function(word){
+//   this.letters = function(){
+//     for (var x = 0; x < word.length; x++){
+//       return new Letter(word.charAt(x));
+//     }
+//   };
+//   this.userDisplay = function(){
+//     for (var x = 0; x < this.letters.length; x++){
+//       this.letters[x].output();
+//     }
+//   };
+//   this.guessLetter = function(char){
+//     for (var x = 0; x < this.letters.length; x++){
+//       this.letters[x].makeGuess(char);
+//     }
+//   }
+// }
